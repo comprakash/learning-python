5e01454737d2bd172e78b4127c914f576b8d7b14
Om Prakash <om.prakash@sony.com>
2020-09-14 11:38:29 +0530

    Updated Label and button code

--- learning-tkinter/src/getting_started/hello.py
+++ learning-tkinter/src/getting_started/hello.py
@@ -1,5 +1,17 @@
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
-Label(root, text="Hello, Om!").pack()
+
+Label(root, text='Hello, Om!').pack()
+
+button = ttk.Button(root, text='Click Me')
+button.pack()
+
+button['text'] = 'Press Me'
+button.config(text='Push Me')
+
+print(button)
+print(root)
+
 root.mainloop()
